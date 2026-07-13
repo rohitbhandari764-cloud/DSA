189ba0779dbe0c8ec5fe8ea69d404e8a9941de27
rohitbhandari764-cloud <rohitbhandari764@gmail.com>
2026-07-13 20:24:32 +0530

    Add files via upload

---
+++ array_basics.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     // declatation and initialization of an array 
+//     int arr[5];  // declaration of an array 
+//     arr[0]=23;   // theae all are initialization 
+//     arr[1]=42;
+//     arr[2]=67;
+//     arr[3]=44;
+//     arr[4]=97;
+
+
+// cout<<arr[0]<<endl;}
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    // declatation and initialization of an array 
+    int arr[5]={12,34,66,24,86};  // both declaration and initialization of an array is done at one time 
+
+    for(int i=0 ; i<=4 ; i++){
+         cout<<arr[i]<<endl;
+    }
+
+}
+
+
---
+++ input_arr.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int arr[5];
+
+   //input
+   for(int i=0 ; i<=4 ; i++){ 
+    cin>>arr[i];
+   }
+
+   //output
+    for(int i=0 ; i<=4 ; i++){ 
+    cout<<arr[i]<<","<<endl;
+   }
+
+
+}
+
+
